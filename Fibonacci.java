247477d5b5a5db95559c396b5119c4a83b1ec512
Irpan Setiadi <irpansetiadi@outlook.co.id>
2016-03-30 10:31:18 +0700

    Added files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,28 @@
+package javaapplication1;
+import java.util.Scanner;
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input : ");
+        int n = input.nextInt();
+        int count=1;
+        int c=1,b=1,a=0;
+        while(count < n){
+            if (count == 1){
+                System.out.print(c+" ");
+            }
+            c=b+a;
+            System.out.print(c+" ");
+            
+            a=b;
+            b=c;
+        
+            count++;
+        }
+        System.out.println();
+    }
+}
